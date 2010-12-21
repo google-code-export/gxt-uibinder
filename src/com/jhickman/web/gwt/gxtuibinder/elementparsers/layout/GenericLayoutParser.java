48ca7aae995caab0d7159928010d70545b79f0c9
justin <justin@jhickman.com>
2010-12-21 00:58:53 -0600

    Making the GenericLayoutParser superclass handle the <gxt:layoutdata>  elements.

--- src/com/jhickman/web/gwt/gxtuibinder/elementparsers/layout/GenericLayoutParser.java
+++ src/com/jhickman/web/gwt/gxtuibinder/elementparsers/layout/GenericLayoutParser.java
@@ -3,10 +3,13 @@
  */
 package com.jhickman.web.gwt.gxtuibinder.elementparsers.layout;
 
+import java.util.Collection;
+
 import com.google.gwt.core.ext.UnableToCompleteException;
 import com.google.gwt.core.ext.typeinfo.JClassType;
 import com.google.gwt.uibinder.rebind.UiBinderWriter;
 import com.google.gwt.uibinder.rebind.XMLElement;
+import com.google.gwt.uibinder.rebind.XMLElement.Interpreter;
 
 /**
  * @author hickman
@@ -14,7 +17,7 @@ import com.google.gwt.uibinder.rebind.XMLElement;
  */
 public class GenericLayoutParser implements LayoutParser {
 	
-	private final String layoutClassName;
+	protected final String layoutClassName;
 
 	public GenericLayoutParser(String layoutClassName) {
 		this.layoutClassName = layoutClassName;
@@ -23,5 +26,22 @@ public class GenericLayoutParser implements LayoutParser {
 	@Override
 	public void parse(XMLElement elem, String fieldName, JClassType type, UiBinderWriter writer) throws UnableToCompleteException {
 		writer.addStatement("%s.setLayout(new %s());", fieldName, layoutClassName);
+	}
+	
+	protected Collection<XMLElement> consumeChildLayoutData(XMLElement elem) throws UnableToCompleteException {
+		Interpreter<Boolean> interpreter = new LayoutDataInterpreter(elem.getNamespaceUri());
+		return elem.consumeChildElements(interpreter);
+	}
+	
+	private static final class LayoutDataInterpreter implements Interpreter<Boolean> {
+		private final String namespaceUri;
+		public LayoutDataInterpreter(String namespaceUri) {
+			this.namespaceUri = namespaceUri;
+		}
+		@Override
+		public Boolean interpretElement(XMLElement elem) throws UnableToCompleteException {
+			return namespaceUri.equals(elem.getNamespaceUri())
+				&& "layoutdata".equals(elem.getLocalName());
+		}
 	}
 }
