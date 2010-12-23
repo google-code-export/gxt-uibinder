9513d3cbea821a6b3ab14cfefb17783f3f758092
justin <justin@jhickman.com>
2010-12-22 22:15:32 -0600

    adding new test project

---
+++ gxt-uibinder-test/src/com/jhickman/web/gwt/gxtuibindertest/client/GxtUiBinderTest.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package com.jhickman.web.gwt.gxtuibindertest.client;
+
+import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.user.client.Window;
+
+/**
+ * @author hickman
+ *
+ */
+public class GxtUiBinderTest implements EntryPoint {
+
+	@Override
+	public void onModuleLoad() {
+	}
+}
