823b00a68db59ea495ff166c9771dee86421d03a
justin <justin@jhickman.com>
2011-02-21 22:31:26 -0600

    adding UiBinder for the SourceView to fix some of the issues with displaying the source code. Changed DemoView to add an AUTO scroll when the window size is smaller than demo area.

--- gxt-uibinder-test/src/com/jhickman/web/gwt/gxtuibindertest/client/view/SourceView.java
+++ gxt-uibinder-test/src/com/jhickman/web/gwt/gxtuibindertest/client/view/SourceView.java
@@ -3,16 +3,14 @@
  */
 package com.jhickman.web.gwt.gxtuibindertest.client.view;
 
-import com.extjs.gxt.ui.client.Style.Scroll;
 import com.extjs.gxt.ui.client.event.ComponentEvent;
 import com.extjs.gxt.ui.client.event.Events;
 import com.extjs.gxt.ui.client.event.Listener;
+import com.extjs.gxt.ui.client.widget.Component;
 import com.extjs.gxt.ui.client.widget.Composite;
 import com.extjs.gxt.ui.client.widget.ContentPanel;
 import com.extjs.gxt.ui.client.widget.Label;
 import com.extjs.gxt.ui.client.widget.LayoutContainer;
-import com.extjs.gxt.ui.client.widget.layout.FlowLayout;
-import com.extjs.gxt.ui.client.widget.layout.MarginData;
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.http.client.Request;
 import com.google.gwt.http.client.RequestBuilder;
@@ -21,15 +19,27 @@ import com.google.gwt.http.client.RequestException;
 import com.google.gwt.http.client.Response;
 import com.google.gwt.safehtml.shared.SafeHtml;
 import com.google.gwt.safehtml.shared.SafeHtmlBuilder;
-import com.google.gwt.user.client.ui.HTMLPanel;
+import com.google.gwt.uibinder.client.UiBinder;
+import com.google.gwt.uibinder.client.UiField;
+import com.google.gwt.user.client.ui.HTML;
 
 /**
  * @author hickman
  *
  */
 public class SourceView extends Composite implements View {
+	
+	static interface Binder extends UiBinder<Component, SourceView> {}
+	private static final Binder BINDER = GWT.create(Binder.class);
+	
+	@UiField LayoutContainer uiBinderSourceContainer;
+	@UiField LayoutContainer javaSourceContainer;
 
 	public SourceView(Class<?> viewClass) {
+		initComponent(BINDER.createAndBindUi(this));
+		
+		
+		
 		
 		String className = viewClass.getName();
 		String[] parts = className.split("\\.");
@@ -39,28 +49,6 @@ public class SourceView extends Composite implements View {
 		
 		String javaSourceUrlString = GWT.getModuleBaseURL() + "view/" + category + "/" + file + ".java";
 		String uiBinderSourceUrlString = GWT.getModuleBaseURL() + "view/" + category + "/" + file + ".ui.xml";		
-		
-		
-		
-		LayoutContainer outerContainer = new LayoutContainer();
-		outerContainer.setLayout(new FlowLayout());
-		outerContainer.setScrollMode(Scroll.AUTOY);
-		
-		
-		ContentPanel container = new ContentPanel();
-		container.setHeaderVisible(false);
-		outerContainer.add(container, new MarginData(10));
-		
-		container.add(new Label("<h1>UiBinder Source</h1>"));
-		final LayoutContainer uiBinderSourceContainer = new LayoutContainer();
-		container.add(uiBinderSourceContainer);
-		
-		container.add(new Label("<br/><br/><h1>Java Source</h1>"));
-		final LayoutContainer javaSourceContainer = new LayoutContainer();
-		container.add(javaSourceContainer);
-		
-		initComponent(outerContainer);
-		
 		
 		getSource(javaSourceUrlString, "java", javaSourceContainer);
 		getSource(uiBinderSourceUrlString, "xml", uiBinderSourceContainer);
@@ -73,13 +61,13 @@ public class SourceView extends Composite implements View {
 				@Override
 				public void onResponseReceived(Request request, Response response) {
 					SafeHtml safeHtml = new SafeHtmlBuilder()
-						.appendHtmlConstant("<pre class='brush: " + language + "; toolbar: false; class-name: \"no_y_scroll\"'>")
+						.appendHtmlConstant("<pre class='brush: " + language + "; tab-size: 2; toolbar: false; class-name: \"no_y_scroll\"'>")
 						.appendEscaped(response.getText())
 						.appendHtmlConstant("</pre>")
 						.toSafeHtml();
 					
 					
-					HTMLPanel content = new HTMLPanel(safeHtml);
+					HTML content = new HTML(safeHtml);
 					container.add(content);
 					container.addListener(Events.AfterLayout, new Listener<ComponentEvent>() {
 						@Override
