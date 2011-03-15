a279fda5ac6978d294d2e2e5b8b2f4ed648e510c
justin <justin@jhickman.com>
2011-03-15 01:27:39 -0500

    Issue 11: Adding demo for TreePanel

--- gxt-uibinder-test/src/com/jhickman/web/gwt/gxtuibindertest/client/ExamplesModel.java
+++ gxt-uibinder-test/src/com/jhickman/web/gwt/gxtuibindertest/client/ExamplesModel.java
@@ -9,6 +9,7 @@ import java.util.List;
 import com.extjs.gxt.ui.client.data.ModelData;
 import com.extjs.gxt.ui.client.data.TreeModel;
 import com.jhickman.web.gwt.gxtuibindertest.client.model.Folder;
+import com.jhickman.web.gwt.gxtuibindertest.client.model.Music;
 import com.jhickman.web.gwt.gxtuibindertest.client.model.Navigation;
 import com.jhickman.web.gwt.gxtuibindertest.client.model.Stock;
 import com.jhickman.web.gwt.gxtuibindertest.client.place.MyPlace;
@@ -33,6 +34,10 @@ public class ExamplesModel {
 				new Folder("Grids",
 						new Navigation[] {
 						new Navigation("Basic Grid", new MyPlace(Token.basicgrid), g.basicgrid().getHTML()),
+				}),
+				new Folder("TreePanel",
+						new Navigation[] {
+						new Navigation("Basic Tree", new MyPlace(Token.basictree), g.basictree().getHTML()),
 				}),
 				new Folder("Tabs",
 						new Navigation[] {
@@ -150,5 +155,127 @@ public class ExamplesModel {
 		
 		stocks.add(new Stock("First Data Corporation", "FDC", 32.7, 32.65));
 		return stocks;
+	}
+
+	/**
+	 * @return
+	 */
+	public static Folder getTreeModel() {
+		Folder[] folders = new Folder[] {
+				new Folder("Beethoven", new Folder[] {
+
+						new Folder("Quartets", new Music[] {
+								new Music("Six String Quartets", "Beethoven",
+										"Quartets"),
+								new Music("Three String Quartets", "Beethoven",
+										"Quartets"),
+								new Music("Grosse Fugue for String Quartets",
+										"Beethoven", "Quartets"), }),
+						new Folder("Sonatas", new Music[] {
+								new Music("Sonata in A Minor", "Beethoven",
+										"Sonatas"),
+								new Music("Sonata in F Major", "Beethoven",
+										"Sonatas"), }),
+
+						new Folder("Concertos",
+								new Music[] {
+										new Music("No. 1 - C", "Beethoven",
+												"Concertos"),
+										new Music("No. 2 - B-Flat Major",
+												"Beethoven", "Concertos"),
+										new Music("No. 3 - C Minor",
+												"Beethoven", "Concertos"),
+										new Music("No. 4 - G Major",
+												"Beethoven", "Concertos"),
+										new Music("No. 5 - E-Flat Major",
+												"Beethoven", "Concertos"), }),
+
+						new Folder("Symphonies", new Music[] {
+								new Music("No. 1 - C Major", "Beethoven",
+										"Symphonies"),
+								new Music("No. 2 - D Major", "Beethoven",
+										"Symphonies"),
+								new Music("No. 3 - E-Flat Major", "Beethoven",
+										"Symphonies"),
+								new Music("No. 4 - B-Flat Major", "Beethoven",
+										"Symphonies"),
+								new Music("No. 5 - C Minor", "Beethoven",
+										"Symphonies"),
+								new Music("No. 6 - F Major", "Beethoven",
+										"Symphonies"),
+								new Music("No. 7 - A Major", "Beethoven",
+										"Symphonies"),
+								new Music("No. 8 - F Major", "Beethoven",
+										"Symphonies"),
+								new Music("No. 9 - D Minor", "Beethoven",
+										"Symphonies"), }), }),
+				new Folder(
+						"Brahms",
+						new Folder[] {
+								new Folder(
+										"Concertos",
+										new Music[] {
+												new Music("Violin Concerto",
+														"Brahms", "Concertos"),
+												new Music(
+														"Double Concerto - A Minor",
+														"Brahms", "Concertos"),
+												new Music(
+														"Piano Concerto No. 1 - D Minor",
+														"Brahms", "Concertos"),
+												new Music(
+														"Piano Concerto No. 2 - B-Flat Major",
+														"Brahms", "Concertos"), }),
+								new Folder(
+										"Quartets",
+										new Music[] {
+												new Music(
+														"Piano Quartet No. 1 - G Minor",
+														"Brahms", "Quartets"),
+												new Music(
+														"Piano Quartet No. 2 - A Major",
+														"Brahms", "Quartets"),
+												new Music(
+														"Piano Quartet No. 3 - C Minor",
+														"Brahms", "Quartets"),
+												new Music(
+														"String Quartet No. 3 - B-Flat Minor",
+														"Brahms", "Quartets"), }),
+								new Folder(
+										"Sonatas",
+										new Music[] {
+												new Music(
+														"Two Sonatas for Clarinet - F Minor",
+														"Brahms", "Sonatas"),
+												new Music(
+														"Two Sonatas for Clarinet - E-Flat Major",
+														"Brahms", "Sonatas"), }),
+								new Folder("Symphonies", new Music[] {
+										new Music("No. 1 - C Minor", "Brahms",
+												"Symphonies"),
+										new Music("No. 2 - D Minor", "Brahms",
+												"Symphonies"),
+										new Music("No. 3 - F Major", "Brahms",
+												"Symphonies"),
+										new Music("No. 4 - E Minor", "Brahms",
+												"Symphonies"), }), }),
+				new Folder("Mozart", new Folder[] { new Folder("Concertos",
+						new Music[] {
+								new Music("Piano Concerto No. 12", "Mozart",
+										"Concertos"),
+								new Music("Piano Concerto No. 17", "Mozart",
+										"Concertos"),
+								new Music("Clarinet Concerto", "Mozart",
+										"Concertos"),
+								new Music("Violin Concerto No. 5", "Mozart",
+										"Concertos"),
+								new Music("Violin Concerto No. 4", "Mozart",
+										"Concertos"), }), }), };
+		Folder root = new Folder("root");
+		for (int i = 0; i < folders.length; i++) {
+			root.add((Folder) folders[i]);
+		}
+
+		return root;
 	}
 }
