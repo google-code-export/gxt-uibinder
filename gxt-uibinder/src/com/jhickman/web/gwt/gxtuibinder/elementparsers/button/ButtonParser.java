3f620ed5e4f1f210035b9d49d9081124383641c1
justin <justin@jhickman.com>
2011-03-17 23:47:18 -0500

    Issue 15: Support other Menu types in Buttons

--- gxt-uibinder/src/com/jhickman/web/gwt/gxtuibinder/elementparsers/button/ButtonParser.java
+++ gxt-uibinder/src/com/jhickman/web/gwt/gxtuibinder/elementparsers/button/ButtonParser.java
@@ -12,6 +12,7 @@ import com.google.gwt.uibinder.rebind.UiBinderWriter;
 import com.google.gwt.uibinder.rebind.XMLElement;
 import com.google.gwt.uibinder.rebind.XMLElement.Interpreter;
 import com.jhickman.web.gwt.gxtuibinder.elementparsers.GxtClassnameConstants;
+import com.jhickman.web.gwt.gxtuibinder.elementparsers.util.SimpleInterpreter;
 
 /**
  * @author hickman
@@ -21,12 +22,35 @@ public class ButtonParser implements ElementParser {
 
 	@Override
 	public void parse(XMLElement elem, String fieldName, JClassType type, UiBinderWriter writer) throws UnableToCompleteException {
-		Interpreter<Boolean> menuInterpreter = new MenuInterpreter(elem.getNamespaceUri());
+		handleBackwardsCompatibleMenu(elem, fieldName, writer);
+		
+		JClassType menuType = writer.getOracle().findType(GxtClassnameConstants.MENU);
+		boolean foundSingleMenu = false;
+		for (XMLElement child : elem.consumeChildElements()) {
+			if (foundSingleMenu) {
+				writer.die(elem, "Buttons only support a single Menu.  Found more than one.");
+			}
+			
+			JClassType childType = writer.findFieldType(child);
+			if ( ! childType.isAssignableTo(menuType)) {
+				writer.die(elem, "Buttons only support nested Menu Components.  Found %s.", child);
+			}
+			foundSingleMenu = true;
+			
+			String menu = writer.parseElementToField(child);
+			writer.addStatement("%s.setMenu(%s);", fieldName, menu);
+		}
+	}
+
+	@Deprecated
+	private void handleBackwardsCompatibleMenu(XMLElement elem,	String fieldName, UiBinderWriter writer) throws UnableToCompleteException {
+		Interpreter<Boolean> menuInterpreter = new SimpleInterpreter(elem.getNamespaceUri(), "menu");
 		
 		Collection<XMLElement> menuChildElements = elem.consumeChildElements(menuInterpreter);
 		if (menuChildElements.isEmpty()) {
 			return;
 		}
+		writer.warn(elem, "Use of <%s:menu> has been deprecated.  Nesting a Menu widget is the new preferred approach.", elem.getPrefix());
 		if (menuChildElements.size() > 1) {
 			writer.die(elem, "Buttons can contain only a single menu.");
 		}
@@ -41,22 +65,5 @@ public class ButtonParser implements ElementParser {
 		
 
 		writer.addStatement("%s.setMenu(%s);", fieldName, menu);
-	}
-	
-	private static final class MenuInterpreter implements Interpreter<Boolean> {
-		private final String namespaceUri;
-		private static final String MENU_LOCALNAME = "menu";
-		
-		public MenuInterpreter(String namespaceUri) {
-			this.namespaceUri = namespaceUri;
-		}
-
-		@Override
-		public Boolean interpretElement(XMLElement elem) throws UnableToCompleteException {
-			if (namespaceUri != null && namespaceUri.equals(elem.getNamespaceUri())) {
-				return MENU_LOCALNAME.equals(elem.getLocalName());
-			}
-			return false;
-		}
 	}
 }
