5c186bab7ebf747486d5562b3a2d66a0b4c848af
justin <justin@jhickman.com>
2011-03-26 00:31:56 -0500

    Issue 20: 	Need the SimpleComboBox to accept attribute values
    Supporting child elements: <form:item value=".." />
    Also supporting types other than String.

--- gxt-uibinder-test/src/com/jhickman/web/gwt/gxtuibindertest/client/view/work/WorkInProgressView.java
+++ gxt-uibinder-test/src/com/jhickman/web/gwt/gxtuibindertest/client/view/work/WorkInProgressView.java
@@ -7,6 +7,7 @@ import com.extjs.gxt.ui.client.widget.Component;
 import com.extjs.gxt.ui.client.widget.Composite;
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.uibinder.client.UiBinder;
+import com.jhickman.web.gwt.gxtuibindertest.client.model.Music;
 import com.jhickman.web.gwt.gxtuibindertest.client.view.View;
 
 /**
@@ -18,8 +19,53 @@ public class WorkInProgressView extends Composite implements View {
 	static interface Binder extends UiBinder<Component, WorkInProgressView> {}
 	private static Binder BINDER = GWT.create(Binder.class);
 	
+	
+	
 	public WorkInProgressView() {
 		initComponent(BINDER.createAndBindUi(this));
 	}
 
+	
+	public static final class Issue20Constants {
+		public String before() {
+			return "before";
+		}
+		public String after() {
+			return "after";
+		}
+		
+		public Person item1() {
+			return new Person("Beethoven");
+		}
+		
+		public Person item2() {
+			return new Person("Brahms");
+		}
+
+		public Person item3() {
+			return new Person("Mozart");
+		}
+
+	}
+	
+	public static final class Person {
+		private String name;
+		
+		public Person(String name) {
+			this.name = name;
+		}
+
+		public String getName() {
+			return name;
+		}
+
+		public void setName(String name) {
+			this.name = name;
+		}
+		
+		@Override
+		public String toString() {
+			return name;
+		}
+	}
 }
