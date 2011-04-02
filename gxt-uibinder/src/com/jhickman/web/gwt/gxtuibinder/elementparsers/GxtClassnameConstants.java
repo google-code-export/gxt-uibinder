b8d0e45b84afd01ee71f4a5d96c5fa7ecdf48116
justin <justin@jhickman.com>
2011-04-01 22:02:24 -0500

    refactoring out a LayoutParserFactory rather than using the old enum.  This will allow us to support other Layouts quicker as it's not relying upon a hardcoded list of them.  All will use the GenericLayoutParser by default, unless specified otherwise.

--- gxt-uibinder/src/com/jhickman/web/gwt/gxtuibinder/elementparsers/GxtClassnameConstants.java
+++ gxt-uibinder/src/com/jhickman/web/gwt/gxtuibinder/elementparsers/GxtClassnameConstants.java
@@ -11,62 +11,68 @@ package com.jhickman.web.gwt.gxtuibinder.elementparsers;
  * @author hickman
  */
 public interface GxtClassnameConstants {
-	public static final String COMPONENT = "com.extjs.gxt.ui.client.widget.Component";
+	
+	public static final String GXT_BASE_PACKAGE = "com.extjs.gxt.ui.client.";
+	
+	public static final String COMPONENT = GXT_BASE_PACKAGE + "widget.Component";
 	
 	
-	public static final String LAYOUTREGION ="com.extjs.gxt.ui.client.Style.LayoutRegion";
-	public static final String MARGINS = "com.extjs.gxt.ui.client.util.Margins";
-	public static final String PADDING = "com.extjs.gxt.ui.client.util.Padding";
-	public static final String LAYOUTCONTAINER = "com.extjs.gxt.ui.client.widget.LayoutContainer";
-	public static final String FORMPANEL = "com.extjs.gxt.ui.client.widget.form.FormPanel";
+	public static final String LAYOUTREGION =GXT_BASE_PACKAGE + "Style.LayoutRegion";
+	public static final String MARGINS = GXT_BASE_PACKAGE + "util.Margins";
+	public static final String PADDING = GXT_BASE_PACKAGE + "util.Padding";
+	public static final String LAYOUTCONTAINER = GXT_BASE_PACKAGE + "widget.LayoutContainer";
+	public static final String FORMPANEL = GXT_BASE_PACKAGE + "widget.form.FormPanel";
 	
-	public static final String FORMBUTTONBINDING = "com.extjs.gxt.ui.client.widget.form.FormButtonBinding";
-	
-	public static final String ACCORDIONLAYOUT= "com.extjs.gxt.ui.client.widget.layout.AccordionLayout";
-	public static final String BORDERLAYOUT = "com.extjs.gxt.ui.client.widget.layout.BorderLayout";
-	public static final String FITLAYOUT = "com.extjs.gxt.ui.client.widget.layout.FitLayout";
-	public static final String FLOWLAYOUT = "com.extjs.gxt.ui.client.widget.layout.FlowLayout";
-	public static final String CARDLAYOUT = "com.extjs.gxt.ui.client.widget.layout.CardLayout";
-	public static final String CENTERLAYOUT = "com.extjs.gxt.ui.client.widget.layout.CenterLayout";
-	public static final String ROWLAYOUT = "com.extjs.gxt.ui.client.widget.layout.RowLayout";
-	public static final String VBOXLAYOUT= "com.extjs.gxt.ui.client.widget.layout.VBoxLayout";
-	public static final String HBOXLAYOUT= "com.extjs.gxt.ui.client.widget.layout.HBoxLayout";
-	public static final String FORMLAYOUT = "com.extjs.gxt.ui.client.widget.layout.FormLayout";
-	public static final String ABSOLUTELAYOUT = "com.extjs.gxt.ui.client.widget.layout.AbsoluteLayout";
-	public static final String FILLLAYOUT = "com.extjs.gxt.ui.client.widget.layout.FillLayout";
-	
-	public static final String BORDERLAYOUTDATA = "com.extjs.gxt.ui.client.widget.layout.BorderLayoutData";	
-	public static final String ABSOLUTEDATA = "com.extjs.gxt.ui.client.widget.layout.AbsoluteData";
-	public static final String MARGINDATA = "com.extjs.gxt.ui.client.widget.layout.MarginData";
-	public static final String ROWDATA = "com.extjs.gxt.ui.client.widget.layout.RowData";
-	public static final String FLOWDATA = "com.extjs.gxt.ui.client.widget.layout.FlowData";
-	public static final String FILLDATA = "com.extjs.gxt.ui.client.widget.layout.FillData";
-	public static final String FORMDATA = "com.extjs.gxt.ui.client.widget.layout.FormData";
-	public static final String HBOXLAYOUTDATA = "com.extjs.gxt.ui.client.widget.layout.HBoxLayoutData";
-	public static final String VBOXLAYOUTDATA = "com.extjs.gxt.ui.client.widget.layout.VBoxLayoutData";
-	
-	public static final String MENU = "com.extjs.gxt.ui.client.widget.menu.Menu";
-	
-	public static final String BUTTON = "com.extjs.gxt.ui.client.widget.button.Button";
+	public static final String FORMBUTTONBINDING = GXT_BASE_PACKAGE + "widget.form.FormButtonBinding";
 	
 	
-	public static final String EVENTTYPE = "com.extjs.gxt.ui.client.event.EventType";
-	public static final String EVENTS = "com.extjs.gxt.ui.client.event.Events";
-	public static final String LISTENER = "com.extjs.gxt.ui.client.event.Listener";
+	public static final String LAYOUT_BASE_PACKAGE = GXT_BASE_PACKAGE + "widget.layout.";
+	
+	public static final String ACCORDIONLAYOUT= LAYOUT_BASE_PACKAGE + "AccordionLayout";
+	public static final String BORDERLAYOUT = LAYOUT_BASE_PACKAGE + "BorderLayout";
+	public static final String FITLAYOUT = LAYOUT_BASE_PACKAGE + "FitLayout";
+	public static final String FLOWLAYOUT = LAYOUT_BASE_PACKAGE + "FlowLayout";
+	public static final String CARDLAYOUT = LAYOUT_BASE_PACKAGE + "CardLayout";
+	public static final String CENTERLAYOUT = LAYOUT_BASE_PACKAGE + "CenterLayout";
+	public static final String ROWLAYOUT = LAYOUT_BASE_PACKAGE + "RowLayout";
+	public static final String VBOXLAYOUT= LAYOUT_BASE_PACKAGE + "VBoxLayout";
+	public static final String HBOXLAYOUT= LAYOUT_BASE_PACKAGE + "HBoxLayout";
+	public static final String FORMLAYOUT = LAYOUT_BASE_PACKAGE + "FormLayout";
+	public static final String ABSOLUTELAYOUT = LAYOUT_BASE_PACKAGE + "AbsoluteLayout";
+	public static final String FILLLAYOUT = LAYOUT_BASE_PACKAGE + "FillLayout";
+	
+	public static final String BORDERLAYOUTDATA = LAYOUT_BASE_PACKAGE + "BorderLayoutData";	
+	public static final String ABSOLUTEDATA = LAYOUT_BASE_PACKAGE + "AbsoluteData";
+	public static final String MARGINDATA = LAYOUT_BASE_PACKAGE + "MarginData";
+	public static final String ROWDATA = LAYOUT_BASE_PACKAGE + "RowData";
+	public static final String FLOWDATA = LAYOUT_BASE_PACKAGE + "FlowData";
+	public static final String FILLDATA = LAYOUT_BASE_PACKAGE + "FillData";
+	public static final String FORMDATA = LAYOUT_BASE_PACKAGE + "FormData";
+	public static final String HBOXLAYOUTDATA = LAYOUT_BASE_PACKAGE + "HBoxLayoutData";
+	public static final String VBOXLAYOUTDATA = LAYOUT_BASE_PACKAGE + "VBoxLayoutData";
+	
+	public static final String MENU = GXT_BASE_PACKAGE + "widget.menu.Menu";
+	
+	public static final String BUTTON = GXT_BASE_PACKAGE + "widget.button.Button";
+	
+	
+	public static final String EVENTTYPE = GXT_BASE_PACKAGE + "event.EventType";
+	public static final String EVENTS = GXT_BASE_PACKAGE + "event.Events";
+	public static final String LISTENER = GXT_BASE_PACKAGE + "event.Listener";
 
-	public static final String TREESTORE = "com.extjs.gxt.ui.client.store.TreeStore";
+	public static final String TREESTORE = GXT_BASE_PACKAGE + "store.TreeStore";
 
-	public static final String LISTSTORE = "com.extjs.gxt.ui.client.store.ListStore";
-	public static final String COLUMNMODEL = "com.extjs.gxt.ui.client.widget.grid.ColumnModel";
-	public static final String COLUMNCONFIG = "com.extjs.gxt.ui.client.widget.grid.ColumnConfig";
+	public static final String LISTSTORE = GXT_BASE_PACKAGE + "store.ListStore";
+	public static final String COLUMNMODEL = GXT_BASE_PACKAGE + "widget.grid.ColumnModel";
+	public static final String COLUMNCONFIG = GXT_BASE_PACKAGE + "widget.grid.ColumnConfig";
 
-	public static final String TOOLTIPCONFIG = "com.extjs.gxt.ui.client.widget.tips.ToolTipConfig";
-
-
-	public static final String STYLEORIENTATION = "com.extjs.gxt.ui.client.Style.Orientation";
+	public static final String TOOLTIPCONFIG = GXT_BASE_PACKAGE + "widget.tips.ToolTipConfig";
 
 
-	public static final String TREEPANEL = "com.extjs.gxt.ui.client.widget.treepanel.TreePanel";
+	public static final String STYLEORIENTATION = GXT_BASE_PACKAGE + "Style.Orientation";
 
-	public static final String HEADER = "com.extjs.gxt.ui.client.widget.Header";
+
+	public static final String TREEPANEL = GXT_BASE_PACKAGE + "widget.treepanel.TreePanel";
+
+	public static final String HEADER = GXT_BASE_PACKAGE + "widget.Header";
 }
